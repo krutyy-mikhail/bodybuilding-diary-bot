86af67f308b150271944645ce812d86a53f8314b
Krutyy Mikhail <krutyy.mikhail@gmail.com>
2020-03-06 11:29:58 +0300

    Add getter and setter for attribute _dao

--- services/services.py
+++ services/services.py
@@ -1,8 +1,24 @@
 from dao import dao
 
 
-class UserService:
-    dao = dao.UserDAO()
+class AbstractService:
+    _dao = None
+
+    @property
+    def dao(self):
+        return self._dao
+
+    @dao.setter
+    def dao(self, value):
+        class_ = type(self.dao)
+        if not isinstance(value, class_):
+            raise TypeError('Attribute "dao" must be object type of "{}".'.format(
+                class_.__name__))
+        self._dao = value
+
+
+class UserService(AbstractService):
+    _dao = dao.UserDAO()
 
     def create(self, user):
         self.dao.create(user)
@@ -11,13 +27,13 @@ class UserService:
         return self.dao.get_user_by_id(user_id)
 
 
-class FoodReportService:
-    dao = dao.FoodReportDAO()
+class FoodReportService(AbstractService):
+    _dao = dao.FoodReportDAO()
 
 
-class BodyReportService:
-    dao = dao.BodyReportDAO()
+class BodyReportService(AbstractService):
+    _dao = dao.BodyReportDAO()
 
 
-class NormalFoodService:
-    dao = dao.NormalFoodDAO()
+class NormalFoodService(AbstractService):
+    _dao = dao.NormalFoodDAO()
